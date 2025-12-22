8e7c1e2ff18572b2a7871845a57a17d27085cde6
xiinq-u <hidayatarief647@gmail.com>
2025-12-22 12:36:59 +0700

    Add Firestore functions import and initialize Firestore in firebase-wishes.js

--- firebase-wishes.js
+++ firebase-wishes.js
@@ -1,3 +1,30 @@
+// 1. Impor fungsi-fungsi yang dibutuhkan dari CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// 2. Konfigurasi Firebase (Dapatkan ini dari Firebase Console -> Project Settings)
+  const firebaseConfig = {
+    apiKey: "AIzaSyB3IfMrJrCwO5aDrvAf0RCU90-AA9E3dwY",
+    authDomain: "undangan-pernikahan0.firebaseapp.com",
+    projectId: "undangan-pernikahan0",
+    storageBucket: "undangan-pernikahan0.firebasestorage.app",
+    messagingSenderId: "1077031359212",
+    appId: "1:1077031359212:web:5b4430b4a2f56ab97a0bc9",
+    measurementId: "G-CX8PYXM9BY"
+  };
+
+// 3. Inisialisasi Firebase & Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 document.addEventListener("DOMContentLoaded", () => {
 
   const form = document.getElementById("wishForm");
